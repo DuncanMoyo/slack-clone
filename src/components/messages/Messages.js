1f23364a9574e17e6dc4dcc6d5e4c7e4f941782c
Duncan Moyo <duncanfmoyo@gmail.com>
2020-10-24 21:37:37 +0200

    display messages & created message component

--- src/components/messages/Messages.js
+++ src/components/messages/Messages.js
@@ -1,41 +1,76 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-import {Segment, Comment} from 'semantic-ui-react'
-import MessageForm from './MessageForm'
-import MessagesHeader from './MessagesHeader'
+import { Segment, Comment } from "semantic-ui-react";
+import MessageForm from "./MessageForm";
+import MessagesHeader from "./MessagesHeader";
 
-import firebase from '../../Firebase'
+import firebase from "../../Firebase";
+import Message from "./Message";
 
 class Messages extends Component {
-
   state = {
-    messagesRef: firebase.database().ref('messages'),
+    messagesRef: firebase.database().ref("messages"),
     channel: this.props.currentChannel,
-    user: this.props.currentUser
+    user: this.props.currentUser,
+    messages: [],
+    messagesLoading: true,
+  };
+
+  componentDidMount() {
+    const { channel, user } = this.state;
+
+    if (channel && user) {
+      this.addListeners(channel.id);
+    }
   }
 
-  render() {
+  addListeners = (channelId) => {
+    this.addMessageListener(channelId);
+  };
 
-    const {messagesRef, channel, user} = this.state
+  addMessageListener = (channelId) => {
+    let loadedMessages = [];
+    this.state.messagesRef.child(channelId).on("child_added", (snap) => {
+      loadedMessages.push(snap.val());
+      // console.log(loadedMessages);
+      this.setState({
+        messages: loadedMessages,
+        messagesLoading: false,
+      });
+    });
+  };
+
+  displayMessages = (messages) => (
+    messages.length > 0 &&
+      messages.map((message) => (
+        <Message
+          key={message.timestamp}
+          message={message}
+          user={this.state.user}
+        />
+      ))
+  );
+
+  render() {
+    const { messagesRef, messages, channel, user } = this.state;
 
     return (
       <>
         <MessagesHeader />
 
         <Segment>
-          <Comment.Group className='messages'>
-            {/* Messages */}
-
+          <Comment.Group className="messages">
+            {this.displayMessages(messages)}
           </Comment.Group>
         </Segment>
-        <MessageForm 
+        <MessageForm
           messagesRef={messagesRef}
           currentChannel={channel}
           currentUser={user}
         />
       </>
-    )
+    );
   }
 }
 
-export default Messages
+export default Messages;
